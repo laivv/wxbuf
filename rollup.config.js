d4d7bce31bacda6564ebf170c995154ea0b8413b
lingluo <lingluo@uoko.com>
2023-12-06 14:50:50 +0800

    feat: add license

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,23 @@
 import terser from '@rollup/plugin-terser'
-import copy from 'rollup-plugin-copy'
-// import extensions from '@rollup/plugin-extensions'
-// import babel from '@rollup/plugin-babel'
-// import obfuscator from 'rollup-plugin-obfuscator'
+// import copy from 'rollup-plugin-copy'
+
+const banner =
+  '/*!\n' +
+  ` * wxbuf.js v1.0.0\n` +
+  ` * (c) ${new Date().getFullYear()} lingluo\n` +
+  ' * Released under the MIT License.\n' +
+  ' */'
 
 export default [{
   input: './src/index.js',
   output: {
-    file: './dist/wxbuf.min.js', // 输出文件
+    file: './dist/wxbuf.min.js',
     format: 'es', // 输出格式 amd / es / cjs / iife / umd / system
-    // name:'func',  // 当format为iife和umd时必须提供，将作为全局变量挂在window(浏览器环境)下：window.A=...
-    sourcemap: false  // 生成bundle.js.map文件，方便调试
+    // name:'wxbuf',  // 当format为iife和umd时必须提供，将作为全局变量挂在window(浏览器环境)下：window.A=...
+    sourcemap: false,  // 生成bundle.js.map文件，方便调试
+    banner
   },
   plugins: [
-    // obfuscator({
-    //   options:{
-    //     simplify: true,
-    //     deadCodeInjection: false,
-    //     identifierNamesGenerator: 'mangled',
-    //     controlFlowFlattening: false,
-    //     unicodeEscapeSequence: false,
-    //     rotateStringArray: false,
-    //     transformObjectKeys: true,
-    //     selfDefending: false,
-    //     stringArray: false,
-    //     stringArrayEncoding: []
-    //   }
-    // })
     terser({
       compress: false
     }),
@@ -39,18 +30,18 @@ export default [{
     // })
   ],
 },
-
 {
   input: './src/index.js',
   output: {
     file: './examples/mini-app-demo/lib/wxbuf.min.js',
     format: 'es',
-    sourcemap: false
+    sourcemap: false,
+    banner
   },
   plugins: [
     terser({
       compress: false
-    }),
+    })
   ],
 }
 ]
