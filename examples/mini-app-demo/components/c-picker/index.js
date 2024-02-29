abef45a38d1d149ab983ac6303556a74afd34665
lingluo <lingluo@uoko.com>
2024-02-29 17:35:41 +0800

    examples

--- examples/mini-app-demo/components/c-picker/index.js
+++ examples/mini-app-demo/components/c-picker/index.js
@@ -1,33 +1,51 @@
 Component({
   // 向父组件实例挂载方法
-  exportMethods:{
-    showPicker ({ onOk, ...option }) {
+  exportMethods: {
+    showPicker(option) {
       this.openPicker(option)
       return new Promise((resolve, reject) => {
-        this.data.resolve = resolve
-        this.data.reject = reject
-        this.data.onOk = onOk
+        this.resolve = resolve
+        this.reject = reject
       })
     }
   },
-  data: {
-    visible: false,
-    resolve: null,
-    reject: null
-  },
   methods: {
-    openPicker(option) {
-      this.setData({ visible: true, ...option })
+    openPicker({ onOk, title, items, style = '', maskClose = true, noStyle = false }) {
+      this.onOk = onOk
+      this.setData({
+        visible: true,
+        title,
+        items,
+        noStyle,
+        maskClose,
+        style
+      }, () => {
+        setTimeout(() => {
+          this.setData({ open: true })
+        }, 16)
+      })
     },
     async onSelected({ currentTarget: { dataset: { value } } }) {
-      this.data.resolve && this.data.resolve(value)
-      if (this.data.onOk) {
-        await this.data.onOk(value)
+      this.resolve && this.resolve(value)
+      if (this.onOk) {
+        await this.onOk(value)
       }
-      this.setData({ visible: false })
-      this.data.onOk = null
-      this.data.resolve = null
-      this.data.reject = null
+      this.close()
+    },
+    close() {
+      this.setData({ open: false }, () => {
+        setTimeout(() => {
+          this.setData({ visible: false })
+        }, 300)
+      })
+      this.resolve = null
+      this.reject = null
+      this.onOk = null
+    },
+    handleMaskTap() {
+      if (this.data.maskClose) {
+        this.close()
+      }
     }
   }
 })
--- examples/mini-app-demo/pages/p/index.js
+++ examples/mini-app-demo/pages/p/index.js
@@ -1,8 +1,13 @@
 Page({
   async handleA() {
+    const item = text => `<div style="color: #fff;background: rgb(25, 175, 83);padding: 8px 0;border-bottom: solid 1px #eee;">${text}</div>`
     const value = await this.showPicker({
-      title: '请选择',
-      items: [{ text: '选项1', value: 1 }, { text: '选项2', value: 2 }]
+      noStyle: true,
+      title: '<span style="color: red;font-size: 30px">请选择</span>',
+      items: [
+        { text: item('选项1'), value: 1 },
+        { text: item('选项2'), value: 2 }
+      ]
     })
     this.setData({ value1: value })
   },
