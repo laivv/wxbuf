b20962898d8236a9a1e4b37a9208e8d4f3621f4e
lingluo <lingluo@uoko.com>
2024-04-12 17:34:26 +0800

    examples: add commet

--- examples/mini-app-demo/app.js
+++ examples/mini-app-demo/app.js
@@ -1,9 +1,10 @@
 import wxbuf from './lib/wxbuf.min'
 
+// 定义顶级全局变量，其它page或组件无需import可直接使用
 wxbuf.global.extend('getAppName', function () {
   return '这段文字由app.js中定义的全局方法提供'
 })
-
+// 给所有page实例扩展公共方法
 wxbuf.page.extend({
   showToast(title) {
     wx.showToast({
@@ -12,7 +13,7 @@ wxbuf.page.extend({
     })
   }
 })
-
+// 对小程序的各种生命周期或事件进行监听
 wxbuf.watch({
   onAppLaunch: function (argv) {
     // console.log('onAppLaunch', argv)
@@ -73,6 +74,7 @@ wxbuf.watch({
   },
 })
 
+// 全局配置
 wxbuf.config({
   // 开启自动反序列化url参数
   parseUrlArgs: true,
@@ -84,12 +86,12 @@ wxbuf.config({
   enableGlobalShareTimeline: true,
   // 指定app中哪个key值被作为store数据来管理
   storeKey: 'globalData',
-
 })
 
 App({
-  // 全局注入store到所有页面、组件
+  // 全局注入store到所有页面、组件的实例上
   injectStore: {
+    // 注入到实例的命名空间（前缀）
     namespace: '$store',
     keys: ['appVersion', 'appCount'],
   },
